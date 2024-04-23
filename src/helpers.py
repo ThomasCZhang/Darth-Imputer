7802f0b3361ff928bd99fc07367b36277b40177d
Thomas Zhang <thomaszhang@thomass-mbp.wifi.local.cmu.edu>
2024-04-22 20:49:05 -0400

    Added highest variance feature selection functions

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from sklearn.decomposition import PCA, TruncatedSVD
 
 def EncodeLabels(labels):
   all_labels = np.unique(labels)
@@ -8,22 +9,22 @@ def EncodeLabels(labels):
   encoded = encoder.transform(labels)
   return encoded
 
-def Implement_Random_Masking(data, masking_portion, num_cols, seed):
+def Implement_Random_Masking(data, sample_proportions, feature_proportion, seed):
   '''
   This function takes X and the masking portion and return the new X with the same shape with missing the given portion of random data
   Args:
   - data: full data
-  - Masking portion: float
-  - num_cols: int 
+  - sample_proportions: float
+  - feature_proportion: float 
   - seed: int
   Returns:
   - masked_data: same shape as given data, missing given portion of data
   '''
   
   np.random.seed(seed)
-  cols = np.random.choice(np.arange(data.shape[1]), size=num_cols)
+  cols = np.random.choice(np.arange(data.shape[1]), size=int(feature_proportion*data.shape[1]))
 
-  mask_indices = np.random.rand(*data[:, cols].shape) < masking_portion
+  mask_indices = np.random.rand(*data[:, cols].shape) < sample_proportions
   masked_data = data.copy()
   masked_data_cols = masked_data[:, cols]
   masked_data_cols[mask_indices] = np.nan
@@ -31,3 +32,49 @@ def Implement_Random_Masking(data, masking_portion, num_cols, seed):
 
   return masked_data
 
+def HighestVarianceFeatures(data, feature_proportion):
+  '''
+  This function takes data and the proportion of features to keep and returns a new matrix with the proportion of
+  features with highest variance.
+  Input:
+  data (np.ndarray)
+  feature_proportion (float)
+
+  Returns:
+  the data with reduced number of features
+  '''
+  variances = np.array([np.var(data[:, col]) for col in range(data.shape[1])])
+
+  n_features = round(feature_proportion*data.shape[1])
+
+  indexes = np.argsort(variances)[-n_features:]
+  explained_ratio = np.sum(variances[indexes])/np.sum(variances)
+  print(f'Percentage of explained variance is: {explained_ratio}')
+
+  return data[:, indexes]
+
+def HighestVarianceDecomposition(data, feature_proportion):
+  '''
+  This function takes data and the proportion of features to keep and returns a new matrix with the proportion of
+  pca features with highest variance.
+  Input:
+  data (np.ndarray)
+  feature_proportion (float)
+
+  Returns:
+  the data with reduced number of features
+  '''
+  # data = data/np.max(data, axis =0, keepdims=True)
+  # data = data-np.mean(data, axis = 0, keepdims=True)
+
+  n_features = round(feature_proportion*data.shape[1])
+
+  if n_features <= min(data.shape):
+    decomposer = PCA(n_components=n_features)
+  else:
+    decomposer = TruncatedSVD(n_components=n_features)
+
+  decomposer.fit(data)
+  explained_ratio = np.sum(decomposer.explained_variance_ratio_)
+  print(f'Percentage of explained variance is: {explained_ratio}')
+  return decomposer.transform(data)
